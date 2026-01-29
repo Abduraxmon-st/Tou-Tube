e54e68dabdc49a503e2a3da5d055bea6f3cb94c1
Abduraxmon-st <2442abd72@gmail.com>
2026-01-29 17:36:50 +0500

    bug layout

--- src/app/[locale]/(user)/layout.tsx
+++ src/app/[locale]/(user)/layout.tsx
@@ -9,17 +9,17 @@ export default function UserLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-screen max-h-screen overflow-hidden">
+    <div className="h-screen max-h-screen">
       <div className="relative z-10 sm:mr-1.25 sm:backdrop-blur-3xl bg-mainColor sm:bg-mainColor/80">
         <Navbar />
         <NavbarTabs />
       </div>
       <div className="flex 2md:gap-6">
-        <div className="relative z-10 -mt-14">
+        <div className="hidden sm:relative z-10 -mt-14">
           <Sidebar />
         </div>
         <div className="relative z-9 sm:-mt-28 flex-1 h-max max-h-dvh w-full overflow-y-auto pb-15">
-          <div className="grid gap-4 grid-cols-5 grid-rows-6 h-400">
+          {/* <div className="grid gap-4 grid-cols-5 grid-rows-6 h-400">
             <div className="bg-green-600"></div>
             <div className="bg-blue-600"></div>
             <div className="bg-yellow-600"></div>
@@ -38,7 +38,7 @@ export default function UserLayout({
             <div className="bg-purple-600"></div>
             <div className="bg-cyan-600"></div>
             <div className="bg-red-600"></div>
-          </div>
+          </div> */}
           {children}
         </div>
       </div>
