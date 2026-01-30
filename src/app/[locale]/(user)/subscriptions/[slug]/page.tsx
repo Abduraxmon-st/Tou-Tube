cdb7d6374d65ed1524c3220d074fada7ebc0e330
rovshanbekm <rmirvoxitov@gmail.com>
2026-01-30 14:21:23 +0500

    finished

--- src/app/[locale]/(user)/subscriptions/[slug]/page.tsx
+++ src/app/[locale]/(user)/subscriptions/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import { Reaktor } from "@/assets/images";
 import { Avatar } from "@/components/avatar";
+import { Button } from "@/components/ui/button";
 import { sidebarSubscriptionsSection } from "@/data/sidebar-links";
+import { Dot } from "lucide-react";
 import Image from "next/image";
 
-export const SubscriptionsSlugPage = async ({ params }: { params: { slug: string, icon: string } }) => {
+export const SubscriptionsSlugPage = async ({ params }: { params: { slug: string } }) => {
     const { slug } = await params;
 
     const subscription = sidebarSubscriptionsSection.find(
@@ -17,7 +19,15 @@ export const SubscriptionsSlugPage = async ({ params }: { params: { slug: string
                 <Avatar className="size-40" icon={subscription?.logo ?? ""} />
                 <div className="flex flex-col gap-4">
                     <h2 className="text-[36px] leading-5.5 font-semibold">{subscription?.label}</h2>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui nulla magnam ad sit optio quod ducimus id dolor minus beatae! Ut, suscipit!</p>
+                    <div className="flex items-center text-thirdColor">
+                        <h3 className="font-medium text-white uppercase">@{subscription?.slug}</h3>
+                        <Dot size={12} />
+                        <p className="text-[14px] leading-5">{subscription?.subscriptions} subscriptions</p>
+                        <Dot size={12} />
+                        <p className="text-[14px] leading-5">{subscription?.videos} video</p>
+                    </div>
+                    <h4 className="text-thirdColor w-[560px]">{subscription?.description}</h4>
+                    <Button className="w-29.5 h-9 font-medium text-[14px] leading-4.5 px-4 rounded-[18px] bg-white text-black">Subscraption</Button>
                 </div>
             </div>
         </div>
