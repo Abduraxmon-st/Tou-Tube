38a7de6428770d9ab64602e4843f7ec5db99b574
Abduraxmon-st <2442abd72@gmail.com>
2026-02-01 16:39:17 +0500

    select old version

---
+++ src/components/select/VideosSelect.tsx
@@ -0,0 +1,5 @@
+export const VideosSelect = ({ videoType }: { videoType: boolean }) => {
+  return (
+    <div>VideosSelect</div>
+  )
+}
--- src/components/select/index.ts
+++ src/components/select/index.ts
@@ -1,1 +1,2 @@
 export * from './SubscriptionSelect'
+export * from './VideosSelect'
