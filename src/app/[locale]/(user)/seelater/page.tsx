ee31162793a12977c9407a41124175e38b018e1c
Abduraxmon-st <2442abd72@gmail.com>
2026-02-13 15:25:55 +0500

    mobile padding bug

--- src/app/[locale]/(user)/seelater/page.tsx
+++ src/app/[locale]/(user)/seelater/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { ListCard } from "@/components/card"
+import { RightVideoSection } from "@/components/section/RightVideoSection";
 import { videos } from "@/data/videos"
 import { Equal } from "lucide-react";
 import { useEffect, useRef } from "react";
@@ -11,25 +12,26 @@ const SeeLaterPage = () => {
 
   useEffect(() => {
     if (!containerRef.current) return;
-  
+
     const swapy = createSwapy(containerRef.current, {
       animation: "dynamic",
       dragAxis: "y",
     });
-  
+
     return () => {
       swapy.destroy();
     };
   }, []);
 
   return (
-    <div className="grid grid-cols-3 gap-4">
-      <div className="col-span-1">
+    <div className="relative max-w-7xl mx-auto lg:grid grid-cols-3 gap-4 lg:pt-5">
+      <div className="col-span-1 lg:sticky lg:h-[calc(100vh-120px)] top-5">
+        <RightVideoSection video={likedVideos[0]} title="See later" videoLength={likedVideos.length} />
       </div>
-      <div ref={containerRef} className="col-span-2 flex flex-col mt-4 px-4 lg:px-0 lg:pr-6!">
+      <div ref={containerRef} className="col-span-2 flex flex-col mt-4 px-4 lg:px-0 lg:pr-3!">
         {likedVideos.map((video) => (
           <div key={video.id} data-swapy-slot={video.id}>
-            <div data-swapy-item={video.id} className="hover:bg-secondColor/10 rounded-[12px] p-3">
+            <div data-swapy-item={video.id} className="hover:bg-secondColor/10 rounded-[12px] py-2 lg:p-3!">
               <div className="flex items-center gap-4 w-full">
                 <span className="text-sm font-medium text-thirdColor hidden lg:block cursor-grab"><Equal /></span>
                 <ListCard video={video} coverWidth="max-w-[50%] 2xm:max-w-[30%]" ownerVisible={false} />
